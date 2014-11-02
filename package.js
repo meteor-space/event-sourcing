18b28a133fbe47fba4ca62bb63864a35d8ba5baa
Dominik Guzei <dominik.guzei@gmail.com>
2014-11-02 16:42:58 +0100

    bumps version to 2.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'CQRS and Event Sourcing Infrastructure for Meteor.',
   name: 'space:cqrs',
-  version: '2.1.0',
+  version: '2.1.1',
   git: 'https://github.com/CodeAdventure/space-cqrs.git',
 });
 
