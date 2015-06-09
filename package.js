f3a7b1957c1e7a4185f7a7fbc8bb9edd031d7962
Dominik Guzei <dominik.guzei@gmail.com>
2015-06-09 17:33:28 +0200

    updates to latest space packages

--- package.js
+++ package.js
@@ -11,8 +11,8 @@ Package.onUse(function(api) {
 
   api.use([
     'coffeescript',
-    'space:base@1.3.1',
-    'space:messaging@0.3.0'
+    'space:base@2.1.0',
+    'space:messaging@1.2.1'
   ]);
 
   api.addFiles(['source/server.coffee'], 'server');
@@ -44,7 +44,7 @@ Package.onTest(function(api) {
     'coffeescript',
     'check',
     'space:cqrs',
-    'practicalmeteor:munit@2.0.2',
+    'practicalmeteor:munit@2.1.4',
     'space:testing@1.3.0'
   ]);
 
