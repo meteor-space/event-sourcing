218215ea012dbb9995410fd62944d3c339725e3f
Dominik Guzei <dominik.guzei@gmail.com>
2015-09-07 20:31:08 +0200

    cleans up cruft and bumps version to 1.1.0
    Took the chance to make breaking changes before
    anyone is really using this :-D

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Event Sourcing Infrastructure for Meteor.',
   name: 'space:event-sourcing',
-  version: '1.0.0',
+  version: '1.1.0',
   git: 'https://github.com/meteor-space/event-sourcing.git',
 });
 
