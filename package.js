a75cd15461c36545ba1bab73c41196502cde3bec
Dominik Guzei <dominik.guzei@gmail.com>
2015-09-19 12:33:26 +0200

    bumps version to 1.3.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Event Sourcing Infrastructure for Meteor.',
   name: 'space:event-sourcing',
-  version: '1.3.0',
+  version: '1.3.1',
   git: 'https://github.com/meteor-space/event-sourcing.git',
 });
 
