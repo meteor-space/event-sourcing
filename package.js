b3e3eb59f9a1474c8c732e37ec2e02bcb643abff
Darko Mijić <darko.mijic@ebiz.hr>
2016-01-29 18:29:51 +0100

    Update package configuration and remove mgp dependencies

--- package.js
+++ package.js
@@ -19,7 +19,7 @@ Package.onUse(function(api) {
     'mikowals:batch-insert@1.1.9',
     'fongandrew:find-and-modify@0.2.1',
     'space:base@4.1.1',
-    'space:messaging@3.1.0',
+    'space:messaging@3.1.1',
     'space:domain@0.2.1'
   ]);
 
