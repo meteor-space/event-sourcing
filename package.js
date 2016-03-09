470d71bfbb9573881b49515c022602cac6d8bc02
darko-mijic <darko.mijic@gmail.com>
2016-03-09 15:30:42 +0100

    Update changelog, package configuration and .versions file for release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Event Sourcing Infrastructure for Meteor.',
   name: 'space:event-sourcing',
-  version: '3.0.0',
+  version: '3.0.1',
   git: 'https://github.com/meteor-space/event-sourcing.git'
 });
 
