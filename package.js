aee05238795d34be32c365496c3bd3516e0f6bfa
qejk <qejk@qejk.net>
2016-11-20 11:15:57 +0100

    Removes meteor package dependency mikowals:batch-insert

--- package.js
+++ package.js
@@ -16,7 +16,6 @@ Package.onUse(function(api) {
     'check',
     'mongo',
     'ecmascript',
-    'mikowals:batch-insert@1.1.14',
     'space:base@4.1.3',
     'space:messaging@3.3.1',
     'space:domain@0.2.1'
