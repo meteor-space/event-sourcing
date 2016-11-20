6d47e3d7ef5bf5b6a07d228f0858f999dd306d92
qejk <qejk@qejk.net>
2016-11-20 15:01:02 +0100

    Hello darkness my old friend

--- package.js
+++ package.js
@@ -19,8 +19,6 @@ Package.onUse(function(api) {
     'mikowals:batch-insert@1.1.14',
     'fongandrew:find-and-modify@1.0.0',
     'space:base@4.1.3',
-    'space:messaging@3.3.1',
-    'space:domain@0.2.1'
   ]);
 
   // ========= server =========
